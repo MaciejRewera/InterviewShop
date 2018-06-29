ed796c71dd17b4a0c8c7c99cd422047b39781739
Maciek <ReweraM@gmail.com>
2018-06-29 08:36:41 +0200

    Added Unit Tests

--- src/main/scala/com/rewera/grocery/GrocerySystem.scala
+++ src/main/scala/com/rewera/grocery/GrocerySystem.scala
@@ -1,5 +1,18 @@
 package com.rewera.grocery
 
+sealed trait Fruit
+case object Apple extends Fruit
+case object Orange extends Fruit
+
 object GrocerySystem {
 
+  val productsPrices: Map[Fruit, BigDecimal] = Map(
+    Apple -> BigDecimal(0.6),
+    Orange -> BigDecimal(0.25)
+  )
+
+  def totalCost(items: Seq[Fruit]): BigDecimal = {
+
+    return null
+  }
 }
--- src/test/scala/com/rewera/grocery/GrocerySystemTest.scala
+++ src/test/scala/com/rewera/grocery/GrocerySystemTest.scala
@@ -4,4 +4,21 @@ import org.scalatest.{FlatSpec, Matchers}
 
 class GrocerySystemTest extends FlatSpec with Matchers {
 
+  "A GrocerySystem" should "return 0.6 for Apple and 0.25 for Orange" in {
+    GrocerySystem.totalCost(Seq(Apple)) shouldBe
+      BigDecimal(0.6).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+    GrocerySystem.totalCost(Seq(Orange)) shouldBe
+      BigDecimal(0.25).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+  it should "return 0 for an empty items list" in {
+    GrocerySystem.totalCost(Seq()) shouldBe
+      BigDecimal(0).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+  it should "return 2.05 for items [ Apple, Apple, Orange, Apple ]" in {
+    val products = Seq(Apple, Apple, Orange, Apple)
+    GrocerySystem.totalCost(products) shouldBe
+      BigDecimal(2.05).setScale(2, BigDecimal.RoundingMode.HALF_UP)
+  }
+
+
 }
